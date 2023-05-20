82d257c8f8062d782447bb7d177bc9306f309a63
Richard Vlas <vlasrichard@gmail.com>
2023-05-20 11:08:49 +0000

    added demo

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import gradio as gr
 from transformers import pipeline
 
-summarizer = pipeline("summarization")
+# Load the text summarization pipeline
+summarizer = pipeline("summarization", model="EleutherAI/gpt-j-6B")
 
-def summarize_text(text):
-    summarized = summarizer(text, max_length=100, min_length=30, do_sample=False)[0]
-    return summarized["summary_text"]
+def summarize_text(input_text):
+    # Use the summarization pipeline to generate a summary
+    summary = summarizer(input_text, max_length=100, min_length=30, do_sample=False)
+    return summary[0]['summary_text']
 
-with gr.Interface(summarize_text, 
-                  inputs=gr.inputs.Textbox(lines=10, placeholder="Enter text to summarize..."),
-                  outputs=gr.outputs.Textbox(placeholder="Summary will appear here...")) as iface:
-    iface.launch()
+# Create the Gradio interface
+iface = gr.Interface(fn=summarize_text, 
+                     inputs="text", 
+                     outputs="text", 
+                     title="Text Summarization", 
+                     description="Enter some text and get a summary of it.")
+
+iface.launch()
