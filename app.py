0b8424ec68357c47d5ff6721cb7d6144b270212a
Richard Vlas <vlasrichard@gmail.com>
2023-05-20 11:06:02 +0000

    added demo

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 import gradio as gr
 from transformers import pipeline
 
-# Load the summarization model from Hugging Face
 summarizer = pipeline("summarization")
 
-# Define the summarization function
-def summarize_text(input_text):
-    summarized = summarizer(input_text, max_length=100, min_length=30, do_sample=False)[0]
+def summarize_text(text):
+    summarized = summarizer(text, max_length=100, min_length=30, do_sample=False)[0]
     return summarized["summary_text"]
 
-# Create the Gradio interface
-iface = gr.Interface(
-    fn=summarize_text,
-    inputs=gr.inputs.Textbox(lines=10, placeholder="Enter text to summarize..."),
-    outputs=gr.outputs.Textbox(),
-)
-
-# Run the interface
-iface.launch()
+with gr.Interface(summarize_text, 
+                  inputs=gr.inputs.Textbox(lines=10, placeholder="Enter text to summarize..."),
+                  outputs=gr.outputs.Textbox(placeholder="Summary will appear here...")) as iface:
+    iface.launch()
